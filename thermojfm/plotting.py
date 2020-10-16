676322beb445b66372228bf3c6d81cc862f3903d
Jack Maddox <john.maddox@uky.edu>
2020-10-16 18:53:54 -0500

    Adjust label and arrow placement visually for log axes

--- thermojfm/plotting.py
+++ thermojfm/plotting.py
@@ -9,6 +9,20 @@ plt.rcParams["figure.dpi"] = 100  # 200 e.g. is really fine, but slower
 
 n_points_default = 100
 
+labelprops_default = dict(
+    rotation_mode='anchor',
+    horizontalalignment='center',
+    verticalalignment='bottom',
+    size='9',
+)
+
+arrowprops_default = dict(
+    arrowstyle='fancy',
+)
+
+pointprops_default = dict(
+    markersize=4
+)
 
 class PropertyPlot:
     """ """
@@ -69,6 +83,95 @@ class PropertyPlot:
         self.p_triple = self.props.p_triple
         self.T_critical = self.props.T_critical
         self.p_critical = self.props.p_critical
+    def _merge_line2D_list(self, line_list):
+        if isinstance(line_list,list):
+            xdata = np.array([])
+            ydata = np.array([])
+            for l in line_list:
+                xdata = np.append(xdata,l.get_xdata())
+                ydata = np.append(ydata,l.get_ydata())
+            line = line_list[0]
+            line.set_xdata(xdata)
+            line.set_ydata(ydata)
+            return line
+        else:
+            return line_list
+    
+    def _trim_line2D_data(self, line, axis_lim):
+        line = self._merge_line2D_list(line)
+        xdata = line.get_xdata()
+        ydata = line.get_ydata()
+        for i,val in enumerate(axis_lim):
+            if isinstance(val,Quantity): axis_lim[i]=val.magnitude
+        if isinstance(xdata,Quantity): xdata=xdata.magnitude
+        if isinstance(ydata,Quantity): ydata=ydata.magnitude
+        ind = np.where(np.logical_and(np.logical_and(np.logical_and(
+            xdata>=axis_lim[0],
+            xdata<=axis_lim[1]),
+            ydata>=axis_lim[2]),
+            ydata<=axis_lim[3])
+            )
+        line.set_xdata(xdata[ind])
+        line.set_ydata(ydata[ind])
+        return line
+
+    def _line_pos(self, line, pos=None, xcoor=None, ycoor=None, **kwargs):
+        line = self._merge_line2D_list(line)
+        pos = pos or 0.5
+        ax = line.axes
+        xdata = line.get_xdata()
+        if isinstance(xdata,Quantity): xdata = xdata.magnitude
+        xA,xB = xdata[0], xdata[-1]
+        Delta_x = xB-xA
+        xlim = ax.get_xlim()
+        Delta_xlim = xlim[-1]-xlim[0]
+        Delta_x_ax = abs(Delta_x/Delta_xlim)
+        ydata = line.get_ydata()
+        if isinstance(ydata,Quantity): ydata = ydata.magnitude
+        yA,yB = ydata[0], ydata[-1]
+        Delta_y = yB-yA
+        ylim = ax.get_ylim()
+        Delta_ylim = ylim[-1]-ylim[0]
+        Delta_y_ax = abs(Delta_y/Delta_ylim)
+        xlog = ax.get_xscale() == "log"
+        ylog = ax.get_xscale() == "log"
+        if len(xdata)==2:
+            if xlog or ylog:
+                if Delta_x_ax > Delta_y_ax:
+                    xdata = np.geomspace(xA, xB, 100)
+                    ydata = yA + (yB-yA)/(xB-xA)*xdata
+                else:
+                    ydata = np.geomspace(yA, yB, 100)
+                    xdata = xA + (xB-xA)/(yB-yA)*ydata
+            else:
+                xdata = np.linspace(xA, xB, 100)
+                ydata = np.linspace(yA, yB, 100)
+            start_ind = int(pos*len(xdata))
+        elif xcoor is not None:
+            start_ind = np.argmin(np.absolute(xdata-xcoor))
+        elif ycoor is not None:
+            start_ind = np.argmin(np.absolute(ydata-ycoor))
+        elif Delta_x_ax>Delta_y_ax:
+            if xlog or ylog:
+                xcoor = np.geomspace(xA, xB, 101)[int(pos*100)]
+            else:
+                xcoor = xdata[0] + pos*Delta_x
+            start_ind = np.argmin(np.absolute(xdata-xcoor))
+        else:
+            if xlog or ylog:
+                ycoor = np.geomspace(yA, yB, 101)[int(pos*100)]
+            else:
+                ycoor = ydata[0] + pos*Delta_y
+            start_ind = np.argmin(np.absolute(ydata-ycoor))
+        end_ind = start_ind+1
+        if start_ind >= len(xdata) or end_ind >= len(xdata):
+            start_ind=-2
+            end_ind=-1
+        x1 = xdata[start_ind]
+        y1 = ydata[start_ind]
+        x2 = xdata[end_ind]
+        y2 = ydata[end_ind]
+        return ax,x1,y1,x2,y2
 
     def plot_point(
         self,
@@ -79,7 +182,9 @@ class PropertyPlot:
         color="black",
         label=None,
         label_loc="north",
-        offset=10,
+        offset=5,
+        pointprops={},
+        labelprops={},
         **kwargs,
     ):
         """
@@ -95,6 +200,8 @@ class PropertyPlot:
         :param **kwargs:
 
         """
+        pointprops = {**pointprops_default, **pointprops}
+        labelprops = {**labelprops_default, **labelprops}
         x = x.to(self.x_units).magnitude
         y = y.to(self.y_units).magnitude
         self.ax.plot(x, y, *args, marker=marker, color=color, **kwargs)
@@ -117,12 +224,14 @@ class PropertyPlot:
         self.ax.annotate(
             label,  # this is the text
             (x, y),  # this is the point to label
+            **labelprops,
             textcoords="offset points",  # how to position the text
             xytext=xytext,  # distance from text to points (x,y)
-            ha="left",  # horizontal alignment can be left, right or center
+            ha=ha,  # horizontal alignment can be left, right or center
+            va=va,  # vertical alignment can be top, bottom, or middle
         )
 
-    def plot_state(self, state_dict, *args, **kwargs):
+    def plot_state(self, state_dict, *args, pointprops={}, **kwargs):
         """
 
         :param state_dict:
@@ -130,11 +239,12 @@ class PropertyPlot:
         :param **kwargs:
 
         """
+        pointprops = {**pointprops, **pointprops_default}
         x = state_dict[self.x_symb]
         y = state_dict[self.y_symb]
         if "label" not in kwargs.keys():
             kwargs["label"] = state_dict["ID"]
-        self.plot_point(x, y, *args, **kwargs)
+        self.plot_point(x, y, *args, **kwargs, **pointprops)
 
     def plot_states(self, key, *args, **kwargs):
         if isinstance(key, slice):
@@ -154,6 +264,11 @@ class PropertyPlot:
         alt_range=None,
         n_points=n_points_default,
         verbose=False,
+        pos=None,
+        arrow=False,
+        arrowprops={},
+        label=None,
+        labelprops={},
         **kwargs,
     ):
         """
@@ -172,7 +287,10 @@ class PropertyPlot:
             if len(x_range) == 2:
                 x1 = x_range[0].to(self.x_units).magnitude
                 x2 = x_range[1].to(self.x_units).magnitude
-                x_try = np.linspace(x1, x2, n_points) * units(self.x_units)
+                if self.ax.get_xscale() == "log":
+                    x_try = np.geomspace(x1, x2, num=n_points) * units(self.x_units)
+                else:
+                    x_try = np.linspace(x1, x2, n_points) * units(self.x_units)
                 x = np.array([])
                 y = np.array([])
                 for i in x_try:
@@ -195,7 +313,10 @@ class PropertyPlot:
             if len(y_range) == 2:
                 y1 = y_range[0].to(self.y_units).magnitude
                 y2 = y_range[1].to(self.y_units).magnitude
-                y_try = np.linspace(y1, y2, n_points) * units(self.y_units)
+                if self.ax.get_yscale() == "log":
+                    y_try = np.geomspace(y1, y2, num=n_points) * units(self.y_units)
+                else:
+                    y_try = np.linspace(y1, y2, n_points) * units(self.y_units)
                 x = np.array([])
                 y = np.array([])
                 for i in y_try:
@@ -239,6 +360,10 @@ class PropertyPlot:
             else:
                 print("Expected a list with two values for alt_range")
         isoline = self.ax.plot(x, y, **kwargs)
+        if arrow:
+            self.add_arrow(line=isoline, pos=pos, xcoor=xcoor, ycoor=ycoor, arrowprops=arrowprops, **kwargs)
+        if label is not None:
+            self.label_line(line=isoline, label=label, pos=pos, xcoor=xcoor, ycoor=ycoor, labelprops=labelprops, **kwargs)
         return isoline
 
     def plot_isentropic_efficiency(
@@ -246,7 +371,6 @@ class PropertyPlot:
         begin_state=None,
         end_state=None,
         color="black",
-        arrow=False,
         n_points=n_points_default,
         show_reference=True,
         verbose=False,
@@ -299,10 +423,12 @@ class PropertyPlot:
         path=None,
         iso_symb=None,
         color="black",
+        pos=None,
+        xcoor=None,
+        ycoor=None,
         arrow=True,
-        arrow_pos=0.5,
+        arrowprops={},
         label=None,
-        label_pos=0.5,
         labelprops={},
         **kwargs,
     ):
@@ -435,56 +561,18 @@ class PropertyPlot:
         else:
             process_line = plot_straight_line(color=color, linestyle="--", **kwargs)
         if arrow:
-            self.add_arrow(line=process_line, arrow_pos=arrow_pos, **kwargs)
+            self.add_arrow(line=process_line, pos=pos, xcoor=xcoor, ycoor=ycoor, arrowprops=arrowprops, **kwargs)
         if label is not None:
-            self.label_line(line=process_line, label=label, label_pos=label_pos, labelprops=labelprops, **kwargs)
+            self.label_line(line=process_line, label=label, pos=pos, xcoor=xcoor, ycoor=ycoor, labelprops=labelprops, **kwargs)
         return process_line
-
-    def _line_pos(self, line, pos,**kwargs):
-        if isinstance(line,list):
-            xdata = np.array([])
-            ydata = np.array([])
-            for l in line:
-                xdata = np.append(xdata,l.get_xdata())
-                ydata = np.append(ydata,l.get_ydata())
-            line = line[0]
-            line.set_xdata(xdata)
-            line.set_ydata(ydata)
-        ax = line.axes
-        xdata = line.get_xdata()
-        ydata = line.get_ydata()
-        xlim = ax.get_xlim()
-        Delta_xlim = xlim[-1]-xlim[0]
-        Delta_x = xdata[-1]-xdata[0]
-        Delta_x_mag = abs(Delta_x/Delta_xlim)
-        if isinstance(Delta_x_mag, Quantity): Delta_x_mag = Delta_x_mag.magnitude
-        ylim = ax.get_ylim()
-        Delta_ylim = ylim[-1]-ylim[0]
-        Delta_y = ydata[-1]-ydata[0]
-        Delta_y_mag = abs(Delta_y/Delta_ylim)
-        if isinstance(Delta_y_mag, Quantity): Delta_y_mag = Delta_y_mag.magnitude
-        xpos = xdata[0] + Delta_x*pos
-        ypos = ydata[0] + Delta_y*pos
-        if len(xdata)>2:
-            if Delta_x_mag > Delta_y_mag: 
-                start_ind = np.argmin(np.absolute(xdata-xpos))
-            else:
-                start_ind = np.argmin(np.absolute(ydata-ypos))
-            end_ind = start_ind+1
-            x1 = xdata[start_ind]
-            y1 = ydata[start_ind]
-            x2 = xdata[end_ind]
-            y2 = ydata[end_ind]
-        else:
-            x1 = xpos
-            x2 = x1 + Delta_x*.01
-            y1 = ypos
-            y2 = y1 + Delta_y*.01
-        return ax,x1,y1,x2,y2
     
-    def add_arrow(self, line, arrow_pos=0.5, arrowprops=None,**kwargs):
-        ax,x1,y1,x2,y2 = self._line_pos(line=line, pos=arrow_pos)
-        arrowprops=arrowprops or dict(arrowstyle='fancy')
+    def add_arrow(self, line, pos=None, xcoor=None, ycoor=None, arrowprops={},**kwargs):
+        pos=pos or 0.5
+        arrowprops = {**arrowprops_default, **arrowprops}
+        if 'pos' in arrowprops.keys():
+            pos=arrowprops['pos']
+            del arrowprops['pos']
+        ax,x1,y1,x2,y2 = self._line_pos(line=line, pos=pos, xcoor=xcoor, ycoor=ycoor)
         arrow = ax.annotate('',
             xytext=(x1, y1),
             xy=(x2,y2),
@@ -492,7 +580,7 @@ class PropertyPlot:
         )
         return arrow
 
-    def label_line(self, line, label, label_pos=0.5, rotate=True, labelprops={}, **kwargs):
+    def label_line(self, line, label, pos=None, xcoor=None, ycoor=None, offset=5, rotate=True, labelprops={}, verbose=False, **kwargs):
         """Add a label to a line, optional rotated to be tangent.
 
         Arguments
@@ -505,28 +593,49 @@ class PropertyPlot:
             whether to align the text to the local slope of the line
         size : float
         """
-        default_labelprops=dict(
-            rotation_mode='anchor',
-            horizontalalignment='center',
-            verticalalignment='bottom',
-            size='9')
-        labelprops = {**default_labelprops, **labelprops}
-        ax,x1,y1,x2,y2 = self._line_pos(line, pos=label_pos)
+        pos = pos or 0.5
+        labelprops = {**labelprops_default, **labelprops}
+        if 'pos' in labelprops.keys():
+            pos = labelprops['pos']
+            del labelprops['pos']
+        ax,x1,y1,x2,y2 = self._line_pos(line, pos=pos, xcoor=xcoor, ycoor=ycoor)
         if isinstance(x1,Quantity): x1=x1.magnitude
         if isinstance(y1,Quantity): y1=y1.magnitude
         if isinstance(x2,Quantity): x2=x2.magnitude
         if isinstance(y2,Quantity): y2=y2.magnitude
-        if x1>x2: x1,y1,x2,y1 = x2,y2,x1,y1
+        Delta_x = x2-x1
+        Delta_y = y2-y1
+        if x1>x2:
+            x1,y1,x2,y1 = x2,y2,x1,y1
+            swap=True
+        else: swap=False
         slp1 = ax.transData.transform_point((x1,y1))
         slp2 = ax.transData.transform_point((x2,y2))
         rise = (slp2[1]-slp1[1])
         if isinstance(rise, Quantity): rise = rise.magnitude
         run = (slp2[0]-slp1[0])
+        if swap: rise=-rise
         if isinstance(run, Quantity): run = run.magnitude
         slope_degrees = np.degrees(np.arctan2(rise,run))
+        if 'offset' in labelprops.keys():
+            offset = labelprops['offset']
+            del labelprops['offset']
+        xytext = [0,0]
+        if 'va' in labelprops.keys():
+            labelprops['verticalalignment'] = labelprops['va']
+        va = labelprops['verticalalignment']
+        if va=='top':
+            offset_angle = slope_degrees - 90
+        else:
+            offset_angle = slope_degrees + 90
+        xytext[0] = offset*np.cos(np.deg2rad(offset_angle))
+        xytext[1] = offset*np.sin(np.deg2rad(offset_angle))
+        if verbose:
+            print(f'label: {label}\n  coord: ({x1},{y1}),(x2,y2)\n  angle: {slope_degrees}\n   offset angle: {offset_angle}\n  offset={xytext}')
         text = ax.annotate(label,
-            xytext=(x1, y1),
             xy=(x1,y1),
+            textcoords="offset points",
+            xytext=xytext,
             rotation=slope_degrees,
             **labelprops
         )
@@ -617,10 +726,15 @@ class PropertyPlot:
         preserve_limits=True,
         n_points=n_points_default,
         linewidth=0.5,
+        linestyle=(0, (5,5)),
         color='gray',
         verbose=False,
+        pos=None,
+        xcoor=None,
+        ycoor=None,
+        arrow=False,
+        arrowprops={},    
         label=None,
-        label_pos=0.5,
         labelprops={},
         **kwargs,
     ):
@@ -633,7 +747,7 @@ class PropertyPlot:
         :param **kwargs:
 
         """
-        kwargs = dict(linewidth=linewidth, color=color, **kwargs)
+        kwargs = dict(linestyle=linestyle, linewidth=linewidth, color=color, **kwargs)
         orig_xlim = self.ax.get_xlim()
         orig_ylim = self.ax.get_ylim()
         xmin = Quantity(orig_xlim[0], self.x_units)
@@ -643,25 +757,31 @@ class PropertyPlot:
         try:
             x_f = getattr(self.props, self.x_symb)(T=T, x=0).to(self.x_units)
             x_g = getattr(self.props, self.x_symb)(T=T, x=1).to(self.x_units)
-            if x_f > xmin and x_g < xmax:
-                isoline = []
+            isoline = []
+            x1 = xmin
+            x2 = xmax
+            if xmin < x_f:
                 isoline.append(self.plot_iso_line(
                     "T",
                     T,
-                    x_range=[xmin, x_f],
+                    x_range=[x1, x_f],
                     **kwargs,
                 )[0])
+                x1 = x_f
+            if xmin < x_g:
                 isoline.append(self.plot_iso_line(
                     "T",
                     T,
-                    x_range=[x_f,x_g],
+                    x_range=[x1, x_g],
                     **kwargs,
                 )[0])
+                x1 = x_g
+            if xmax > x_g:
                 isoline.append(self.plot_iso_line(
                     "T",
                     T,
-                    x_range=[x_g,xmax],
-                    **kwargs
+                    x_range=[x1, xmax],
+                    **kwargs,
                 )[0])
         except:
             isoline = self.plot_iso_line(
@@ -670,13 +790,17 @@ class PropertyPlot:
                 x_range = [Quantity(i,self.x_units) for i in orig_xlim],
                 **kwargs,
             )
+        isoline = self._merge_line2D_list(isoline)
         if preserve_limits:
             self.ax.set_xlim(orig_xlim)
             self.ax.set_ylim(orig_ylim)
+            isoline = self._trim_line2D_data(isoline,[xmin,xmax,ymin,ymax])
+        if arrow:
+            self.add_arrow(isoline,pos=pos, xcoor=xcoor, ycoor=ycoor, arrowprops=arrowprops,**kwargs)
         if label:
-            self.label_line(isoline,label=label,label_pos=label_pos,labelprops=labelprops,**kwargs)
-        return isoline
-
+            self.label_line(isoline,label=label,pos=pos, xcoor=xcoor, ycoor=ycoor,labelprops=labelprops,**kwargs)
+        return isoline        
+    
     def plot_isobar(
         self,
         p=None,
@@ -685,10 +809,15 @@ class PropertyPlot:
         preserve_limits=True,
         n_points=n_points_default,
         linewidth=0.5,
+        linestyle=(0, (5,5)),
         color='gray',
         verbose=False,
+        pos=None,
+        xcoor=None,
+        ycoor=None,
+        arrow=False,
+        arrowprops={},    
         label=None,
-        label_pos=0.5,
         labelprops={},
         **kwargs,
     ):
@@ -701,7 +830,7 @@ class PropertyPlot:
         :param **kwargs:
 
         """
-        kwargs = dict(linewidth=linewidth, color=color, **kwargs)
+        kwargs = dict(linestyle=linestyle, linewidth=linewidth, color=color, **kwargs)
         orig_xlim = self.ax.get_xlim()
         orig_ylim = self.ax.get_ylim()
         xmin = Quantity(orig_xlim[0], self.x_units)
@@ -711,7 +840,7 @@ class PropertyPlot:
         try:
             x_f = getattr(self.props, self.x_symb)(p=p, x=0).to(self.x_units)
             x_g = getattr(self.props, self.x_symb)(p=p, x=1).to(self.x_units)
-            if x_f > xmin and x_g < xmax:
+            if x_f > xmin:
                 isoline = []
                 isoline.append(self.plot_iso_line(
                     "p",
@@ -719,12 +848,14 @@ class PropertyPlot:
                     x_range=[xmin, x_f],
                     **kwargs,
                 )[0])
+            if x_g > xmin:
                 isoline.append(self.plot_iso_line(
                     "p",
                     p,
                     x_range=[x_f,x_g],
                     **kwargs,
                 )[0])
+            if x_g < xmax:
                 isoline.append(self.plot_iso_line(
                     "p",
                     p,
@@ -741,8 +872,11 @@ class PropertyPlot:
         if preserve_limits:
             self.ax.set_xlim(orig_xlim)
             self.ax.set_ylim(orig_ylim)
+            isoline = self._trim_line2D_data(isoline,[xmin,xmax,ymin,ymax])
+        if arrow:
+            self.add_arrow(isoline,pos=pos,xcoor=xcoor,ycoor=ycoor,arrowprops=arrowprops,**kwargs)
         if label:
-            self.label_line(isoline,label=label,label_pos=label_pos,labelprops=labelprops,**kwargs)
+            self.label_line(isoline,label=label,pos=pos,xcoor=xcoor,ycoor=ycoor,labelprops=labelprops,**kwargs)
         return isoline
             
     def plot_triple_point(self, label="TP", label_loc="east", **kwargs):
