ee47bd7dcde046ff5b4575d0b1df3f135836071a
Jack Maddox <jackmaddox@gmail.com>
2021-05-10 01:55:43 -0500

    update requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="kilojoule",
-    version="0.2.1",
+    version="0.2.2",
     author="Jack Maddox",
     author_email="jackmaddox@gmail.com",
     description="A convenience package for thermodynamic and heat transfer calculations",
@@ -17,6 +17,16 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'sympy',
+        'pandas',
+        'matplotlib',
+        'pint',
+        'coolprop',
+        'pyromat',
     ],
     python_requires='>=3.6',
 )
